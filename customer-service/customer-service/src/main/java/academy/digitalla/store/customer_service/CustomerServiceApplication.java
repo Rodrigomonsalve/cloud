5d87df3f7d641192aa884117da62ef2cbb0c0256
Rodrigomonsalve <94495911+Rodrigomonsalve@users.noreply.github.com>
2024-10-31 03:15:57 -0600

    31.10.2024

--- customer-service/customer-service/src/main/java/academy/digitalla/store/customer_service/CustomerServiceApplication.java
+++ customer-service/customer-service/src/main/java/academy/digitalla/store/customer_service/CustomerServiceApplication.java
@@ -2,6 +2,7 @@ package academy.digitalla.store.customer_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+ 
 
 @SpringBootApplication
 public class CustomerServiceApplication {
