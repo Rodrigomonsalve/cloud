1a3dcb1b2392cbc68d72c7f70d75691dce6e5dbd
Rodrigomonsalve <94495911+Rodrigomonsalve@users.noreply.github.com>
2024-10-31 04:55:28 -0600

    31.10.2024(3)

--- shopping-service/shopping-service/src/main/java/academy/digitalla/store/shopping_service/ShoppingServiceApplication.java
+++ shopping-service/shopping-service/src/main/java/academy/digitalla/store/shopping_service/ShoppingServiceApplication.java
@@ -2,8 +2,10 @@ package academy.digitalla.store.shopping_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ShoppingServiceApplication {
 
 	public static void main(String[] args) {
