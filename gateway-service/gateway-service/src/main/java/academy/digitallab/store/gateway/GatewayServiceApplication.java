82048ec7358f923829d871db27bfba2e114c00e1
Rodrigomonsalve <94495911+Rodrigomonsalve@users.noreply.github.com>
2024-11-08 03:21:03 -0600

    08.11.2024(3)

--- gateway-service/gateway-service/src/main/java/academy/digitallab/store/gateway/GatewayServiceApplication.java
+++ gateway-service/gateway-service/src/main/java/academy/digitallab/store/gateway/GatewayServiceApplication.java
@@ -4,6 +4,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+
+//ESTE ES EL SERVIDOR (GATEWAY) A TRAVES DEL CUAL SE REALIZARAN TODAS LAS PETICIOES A CUALQUIER MICROSERVICIO.
+//YA NO SERA NECESARIO VISITAR CADA MICROSERVICIO A TRAVES DE SU PUERTO PARTICULAR. TODOS OBEDECERAN A UNO SOLO, QUE ES EL DE ESTE SERVIDOR. EJEMPLO
+//http://localhost:8086/customers
+//http://localhost:8086/products
+
+//FLUJO:
+// 1) Acude al servidor centralizado.
+// 2) El servidor cetralizado localiza en la nube el archivo de propiedades del gateway para inicializarlo, y localizar la url buscada dentro de él (/products,/customers, etc).
+// 3) Acude a Eureka para saber en dónde esta ese microservicio en particular.
+
+//Por lo anterior, forzosamente tambien se debe registar en Eureka.
+
+// NO HAY CÒDIGO. TOD LO HACE EL ARCHIVO DE CONFIGURACION EN LA NUBE, UNA DEPENDENCIA EN EL POM, Y EUREKA.
 @EnableEurekaClient
 @SpringBootApplication
 public class GatewayServiceApplication {
